8a7ce4a9e8b1b386318eebd4b555d5b75c9bfc15
dh <1399801745@qq.com>
2020-10-20 18:07:05 +0800

    代码生成器的数据库连接修改

--- rrk-common/src/main/java/com/rrk/CodeGenerator.java
+++ rrk-common/src/main/java/com/rrk/CodeGenerator.java
@@ -62,7 +62,7 @@ public class CodeGenerator {
         dsc.setDriverName("com.mysql.jdbc.Driver");
         dsc.setUsername("root");
         dsc.setPassword("123456");
-        dsc.setUrl("jdbc:mysql://localhost:3306/boot_order?useUnicode=true&characterEncoding=utf-8&serverTimezone=UTC");
+        dsc.setUrl("jdbc:mysql://local.mysql.com:3306/boot_order?useUnicode=true&characterEncoding=utf-8&serverTimezone=UTC");
         mpg.setDataSource(dsc);
 
         // 策略配置
